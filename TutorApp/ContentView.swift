f6595d2f2c3e7476ef5d44929c3f2c9a0698e7b0
u201314147 <u201314147@upc.edu.pe>
2019-11-02 14:22:28 -0700

    first commit

---
+++ TutorApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TutorApp
+//
+//  Created by PCSIJTIN on 11/2/19.
+//  Copyright © 2019 Profind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
