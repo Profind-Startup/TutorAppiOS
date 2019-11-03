ad9bc0f9382bb509fdce9d8634aed5c123f5da1d
u201314147 <u201314147@upc.edu.pe>
2019-11-02 19:40:39 -0500

    agregado pantalla login

--- TutorApp/ContentView.swift
+++ TutorApp/ContentView.swift
@@ -9,9 +9,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+    
     var body: some View {
-        Text("Hello, World!")
-    }
+          
+          VStack {
+          WelcomeText()
+            UserImage()
+            
+            TextField("Username", text: $username)
+                            .padding()
+                            .background(lightGreyColor)
+                            .cornerRadius(5.0)
+                            .padding(.bottom, 20)
+            SecureField("Password", text: $password)
+                            .padding()
+                            .background(lightGreyColor)
+                            .cornerRadius(5.0)
+                            .padding(.bottom, 20)
+            
+             Button(action: {print("Button tapped")}) {
+            LoginButtonContent()
+                        }
+          }.padding()
+      }
+}
+
+
+struct WelcomeText: View {
+    
+    var body: some View {
+          
+          VStack {
+          return Text("TutorApp")
+              .font(.largeTitle)
+              .fontWeight(.semibold)
+              .padding(.bottom, 20)
+          }
+      }
+}
+
+
+struct UserImage: View {
+    
+    var body: some View {
+          
+          VStack {
+          return Image("userImage")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 250, height: 150)
+          .clipped()
+          .padding(.bottom, 75)
+
+          }
+      }
+}
+
+
+struct LoginButtonContent: View {
+    
+    var body: some View {
+           VStack {
+        
+                      Text("LOGIN")
+                          .font(.headline)
+                          .foregroundColor(.white)
+                          .padding()
+                          .frame(width: 220, height: 60)
+                          .background(Color.green)
+                          .cornerRadius(15.0)
+                  }
+                  .padding()
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ TutorApp/loginViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  loginViewModel.swift
+//  TutorApp
+//
+//  Created by PCSIJTIN on 11/2/19.
+//  Copyright © 2019 Profind. All rights reserved.
+//
+
+import UIKit
+
+class loginViewModel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func performLogin()
+    {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
